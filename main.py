41914a22e0a1624df7eb6aeb3bd40e15ed5b44b7
FlowersRlit <yourstrulygurl@gmail.com>
2023-02-21 07:13:03 +0700

    Added shop information and fixing user input method

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 ### dummy file
 
+### main data
 userBalance = 50000
 currency = "Rp"
+
+### shop logic
+Banana = 20000
+Melon = 41000
+Apple = 13000
+
+print("Current available items on the store:\nBanana - " + currency + str(Banana) + "\nMelon - " + currency +str(Melon) + "\nApple - " + currency + str(Apple))
+print("What would you like to buy?")
+
+userInput = input(">>> ")
