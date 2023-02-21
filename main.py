64db459ad9b86d236196c888273b4c05960cd3b7
FlowersRlit <yourstrulygurl@gmail.com>
2023-02-21 10:32:40 +0700

    Shop variables and input method

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ currency = "Rp"
 Banana = 20000
 Melon = 41000
 Apple = 13000
+purchaseConfirm = "Are you sure you want to buy this item? (y/n)"
+purchaseSuccess = "Thank you for buying"
+cancel = "Transaction cancelled. Exiting..."
+otherInput = "Wrong input!"
 
-print("Current available items on the store:\nBanana - " + currency + str(Banana) + "\nMelon - " + currency +str(Melon) + "\nApple - " + currency + str(Apple))
+print("Current available items on the store:\na. Banana - " + currency + str(Banana) + "\nb. Melon - " + currency +str(Melon) + "\nc. Apple - " + currency + str(Apple))
 print("What would you like to buy?")
 
-userInput = input(">>> ")
+userInput = input(">>> ").lower()
